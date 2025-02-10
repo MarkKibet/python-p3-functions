e18d9f5e5d7c2a252e17f43a1cb35b1f7e779439
MarkKibet <kemboimarkbiwott@gmail.com>
2025-02-11 01:55:21 +0300

    python-functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(name="programmer"):
+    print (f"Hello, {name}!")
+greet_programmer()
 
-def greet(name):
-    pass
+   
+
+
+
+def greet(name="Guido"):
+    print (f"Hello, {name}!")
+greet()
+   
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
-    pass
+    return num1+num2
+print(add(2, 4))
+   
 
 def halve(number):
-    pass
+    if number=="number": 
+      return None
+    return number/2
+        
